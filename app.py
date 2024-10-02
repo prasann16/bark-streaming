3a0caa48459c67abbea1020d3da186a13614b120
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-10-03 04:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from bark import SAMPLE_RATE, generate_audio, preload_models
-from bark import generate_text_semantic, semantic_to_waveform
-import time
+from bark import SAMPLE_RATE, generate_audio, preload_models, generate_text_semantic, semantic_to_waveform
 import numpy as np
 import io
 import base64
-import soundfile as SF
+import soundfile as sf
 import nltk
 
 class InferlessPythonModel:
@@ -13,16 +11,35 @@ class InferlessPythonModel:
         preload_models()
         nltk.download('punkt')
         
-    def infer(self, inputs):
+    def infer(self, inputs, stream_output_handler):
         prompt = inputs["prompt"]
-        audio_array = generate_audio(prompt)
-        buffer = io.BytesIO()
+        speaker = inputs["speaker"]
+        sentences = nltk.sent_tokenize(prompt)
+        for i, sentence in enumerate(sentences):
+            
+            print(f"Processing chunk {i+1}: {sentence}")
+            semantic_response = generate_text_semantic(
+                text=sentence,
+                history_prompt=speaker,  # Example speaker preset
+                temp=0.7,
+                silent=True
+            )
+            audio_array = semantic_to_waveform(
+                semantic_tokens=semantic_response,
+                history_prompt=speaker,  # Use the same speaker preset
+                temp=0.7,
+                silent=True
+            )
+
+            # Write the audio data to the bytes buffer using soundfile
+            sample_rate = 24000  # Bark outputs at 24kHz
+            buffer = io.BytesIO()
+            sf.write(byte_io, audio_array, sample_rate, format='WAV')
+            buffer.seek(0)
+            base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
+            stream_output_handler.send_streamed_output({"generated_audio" : base64_audio})
         
-        sf.write(buffer, audio_array,SAMPLE_RATE, format='WAV')
-        buffer.seek(0)
-        base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
-        
-        return {"generated_audio_base64": base64_audio}
+        stream_output_handler.finalise_streamed_output()
 
     def finalize(self,args):
         self.pipe = None
