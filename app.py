cf536615525971deeaa241ca201ac6e9cf02fc59
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-10-03 06:13:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ class InferlessPythonModel:
             # Write the audio data to the bytes buffer using soundfile
             sample_rate = 24000  # Bark outputs at 24kHz
             buffer = io.BytesIO()
-            sf.write(byte_io, audio_array, sample_rate, format='WAV')
+            sf.write(buffer, audio_array, sample_rate, format='WAV')
             buffer.seek(0)
             base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
             stream_output_handler.send_streamed_output({"generated_audio" : base64_audio})
